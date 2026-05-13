b008481b9aa0017d2bd8b0cf1a6c4c4f65a7f439
Victoria <Vic50595@users.noreply.github.com>
2026-05-13 14:54:35 +0200

    fix home link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  function goToSection(e, id) {
+    e.preventDefault()
+    if (location.pathname !== '/') {
+      navigate('/')
+      setTimeout(() => {
+        document.getElementById(id)?.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      }, 50)
+    } else {
+      document.getElementById(id)?.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }
+
   return (
     <nav>
       <ul>
-        <li><Link to="/#section1">Home :)</Link></li>
-        <li><a href="/#section2">Grid Maker</a></li>
+        <li><a href="/#section1" onClick={e => goToSection(e, 'section1')}>Home :)</a></li>
+        <li><a href="/#section2" onClick={e => goToSection(e, 'section2')}>Grid Maker</a></li>
         <li><Link to="/about">About Me</Link></li>
       </ul>
     </nav>
